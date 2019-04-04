112c88393a7fe5973d63b9a6e64a0ec27f6a99b0
developpeur <developpeur@sio2-slam>
2019-04-04 13:28:08 +0200

    1st Commit

---
+++ src/GsbAndroidBundle/Controller/DefaultController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace GsbAndroidBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@GsbAndroid/Default/index.html.twig');
+    }
+    // Retourne la liste des visiteurs sous forme d'un tableau d'objets Json
+    // Côté client mobile : response sous forme de JSONArray 
+    public function getLesVisiteursAction() { 
+        
+        $rp = $this->getDoctrine()->getManager()->getRepository('GsbAndroidBundle:Visiteur');
+        $lesVisiteurs = $rp->findAll();
+        return new JsonResponse($lesVisiteurs);
+        
+    }
+    
+    // Retourne un visiteur sous forme d'un JSONObject
+    // L'entité Visiteur implémente l'interface JsonSerializable
+    public function getUnVisiteurAction($login, $mdp) {
+        
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('GsbAndroidBundle:Visiteur');
+        $leVisiteur = $repository->findOneBy(array('visLogin'=>$login, 'visMdp'=>$mdp));
+        
+        return new Response($leVisiteur->getVisNom()) ;
+        $response = new JsonResponse($leVisiteur);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    
+    // Retourne une réponse sous forme de JSONArray 
+    public function getLesPraticiensAction() {
+        $em = $this->getDoctrine()->getManager();
+        $rp = $em->getRepository('GsbAndroidBundle:Praticien');
+        $data = $rp->findAll(); //>getArrayResult();
+        if (empty($data)) {
+            return new JsonResponse(['message'=> 'Liste vide'], Response::HTTP_NOT_FOUND);
+        }
+        return new JsonResponse($data);
+               
+    }
+    
+    // Idem : tableau d'objets Json
+    public function getLesPraticiensParVisiteurAction($matricule) {
+        $em = $this->getDoctrine()->getManager();
+        $rp = $em->getRepository('GsbAndroidBundle:Praticien');
+        // Exemple d'utilisation du DQL (plus facile pour récupérer les clés
+        // étrangères
+        $lesPraticiens = $rp->findByPraVisiteurDQL($matricule);
+        
+        return new JsonResponse($lesPraticiens);
+    }
+
+    // Exemple avec le service Serializer de Symfony
+    public function getLesTypesPraticiensAction() {
+        $rp = $this->getDoctrine()->getManager()
+                ->getRepository('GsbAndroidBundle:TypePraticien');
+        
+        $typesPraticiens = $rp->findAll();
+        // Ici, un exemple d'utilisation du service serializer de Symfony
+        // Du coup, pas besoin pas besoin d'implémenter JsonSerializable
+        // dans l'entity TypePraticien
+        $lesTypesPraticiens = $this->get('serializer')
+                ->serialize($typesPraticiens, 'json');
+        
+        return new Response($lesTypesPraticiens); 
+    }
+
+}
+
---
+++ src/GsbAndroidBundle/GsbAndroidBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GsbAndroidBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GsbAndroidBundle extends Bundle
+{
+}
